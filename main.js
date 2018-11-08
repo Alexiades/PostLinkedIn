ed1efbe1de322cbe300e425a847f866e5ae63318
alex alexiades <bnb@macAlex.local>
2018-11-08 10:09:24 +0100

    create packge.json

--- main.js
+++ main.js
@@ -3,4 +3,12 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
-}).listen(8081);
+}).listen(8082);
+/*
+var request = require('request');
+
+request('https://api.linkedin.com/v1/people/~/shares?format=json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body)
+    }
+})*/
